e5f220073520fb22c47e33e34a04c083bcb5cea8
Alex Sayers <alex.sayers@gmail.com>
2017-01-13 14:45:53 +0900

    rebase: Allow --exec to be specified multiple times

--- src/main.rs
+++ src/main.rs
@@ -1724,7 +1724,7 @@ fn rebase(repo: &Repository, m: &ArgMatches) -> Result<()> {
     }).collect::<Result<_>>());
 
     let interactive = m.is_present("interactive");
-    let exec_cmd = m.value_of("exec");
+    let exec_cmds = m.values_of("exec");
     let onto = match m.value_of("onto") {
         None => None,
         Some(onto) => {
@@ -1768,8 +1768,10 @@ fn rebase(repo: &Repository, m: &ArgMatches) -> Result<()> {
     let mut git_rebase_todo = try!(create.open(&git_rebase_todo_filename));
     for mut commit in commits {
         try!(writeln!(git_rebase_todo, "pick {}", try!(commit_obj_summarize(&mut commit))));
-        if let Some(cmd) = exec_cmd {
-            try!(writeln!(git_rebase_todo, "exec {}", cmd));
+        if let Some(ref cmds) = exec_cmds {
+            for cmd in cmds.clone() {
+                try!(writeln!(git_rebase_todo, "exec {}", cmd));
+            }
         }
     }
     if let Some(onto) = onto {
@@ -1997,7 +1999,7 @@ fn main() {
                     .about("Rebase the patch series")
                     .arg_from_usage("[onto] 'Commit to rebase onto'")
                     .arg_from_usage("-i, --interactive 'Interactively edit the list of commits'")
-                    .arg(Arg::from_usage("-x, --exec [cmd] 'Append \"exec <cmd>\" after each line creating a commit in the final history.'").requires("interactive"))
+                    .arg(Arg::from_usage("-x, --exec <cmd> 'Append \"exec <cmd>\" after each line creating a commit in the final history.'").multiple(true).number_of_values(1).requires("interactive"))
                     .group(ArgGroup::with_name("action").args(&["onto", "interactive"]).multiple(true).required(true)),
                 SubCommand::with_name("req")
                     .about("Generate a mail requesting a pull of the patch series")
